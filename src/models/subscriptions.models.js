54bea4cb119da09119c76b4a6c2f7c89d0a499cb
Anees shaikh <aneesshaikh329@gmail.com>
2026-02-02 21:42:48 +0530

    Add getUserChannelDetails controller and fix subscription model export

--- src/models/subscriptions.models.js
+++ src/models/subscriptions.models.js
@@ -15,4 +15,4 @@ const subscriptionSchema = new Schema({
 )
 
 
-export const Subscriptions = mongoose.model('Subscriptions',subscriptionSchema ) 
+export const Subscription = mongoose.model('Subscription',subscriptionSchema ) 
